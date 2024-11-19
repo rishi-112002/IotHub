d2d3c4e88690887096ea36da5ee5054fa71be593
Faizsheikh113 <faizsheikh113@gmail.com>
2024-11-19 13:59:10 +0530

    For pull request

--- src/navigation/DrawerNavigation.tsx
+++ src/navigation/DrawerNavigation.tsx
@@ -17,16 +17,16 @@ export default function DrawerNavigation() {
   const navigation = useNavigation();
   return (
     <Drawer.Navigator
-      initialRouteName={"bottomTabNavigation"}
+      initialRouteName={'bottomTabNavigation'}
       screenOptions={{ headerShown: true }}
       drawerContent={() => <CustomDrawerContent />}>
       <Drawer.Screen name="bottomTabNavigation" component={BottomTabNavigation}  options={{headerShown:false}} />
       <Drawer.Screen name="WeighBridgeNavigation" component={WeighBridgeNavigation} options={{headerShown:false}}  />
       <Drawer.Screen name="GenericSpotNavigation" component={GenericNavigation}  options={{headerShown:false}} />
-      <Drawer.Screen name="RfidScreenNavigation" component={RfidScreenNavigation} 
+      <Drawer.Screen name="RfidScreenNavigation" component={RfidScreenNavigation}
        options={{headerShown:false}} />
       <Drawer.Screen name="LiveSpots" component={HomeNavigation}  options={{headerShown:false}}/>
-   
+
     </Drawer.Navigator>
   );
 }
